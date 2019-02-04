09a44a0411f4f38b40ed9283a12a266fa9d24201
Anonymous <sipayi@gmail.com>
2019-02-03 21:06:32 -0500

    Added repeat user error condition

--- services/src/main/java/com/milindc/ebooks/tracker/service/model/StudentView.java
+++ services/src/main/java/com/milindc/ebooks/tracker/service/model/StudentView.java
@@ -8,6 +8,11 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 public class StudentView {
+	
+	private Long id; 
+	
+	private Long studentId;
+	
 	private String firstName;
 	
 	private String lastName;
@@ -18,7 +23,11 @@ public class StudentView {
 	
 	private String phone2;
 	
-	private Long id; 
+	private Integer enabled = 1;
+	
+	private Integer academicYear;
+	
+	private Integer age;
 	
 	@Getter(AccessLevel.NONE)
 	private String fullName;
@@ -27,12 +36,15 @@ public class StudentView {
 		return String.format("%s, %s %s", lastName, firstName, middleName);
 	}
 
-	public StudentView(String firstName, String lastName, String middleName, Long id) {
+	public StudentView(String firstName, String lastName, String middleName, 
+			Long id, Integer age, Integer academicYear) {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.middleName = middleName;
 		this.id = id;
 		this.fullName = getFullName();
+		this.academicYear = academicYear;
+		this.age = age;
 	}
 	
 }
